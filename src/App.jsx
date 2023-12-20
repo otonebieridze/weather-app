cac4a69409d98110af744b6acc2b5a57a796ef0d
oto <otonebieridze123@gmail.com>
2023-12-20 12:00:10 +0400

    Modify Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-gradient-to-br from-gray-300 to-gray-900">
+    <div className="w-full h-screen bg-gradient-to-br from-gray-300 to-gray-900 flex flex-col items-center">
       <Header />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { IoSearch } from "react-icons/io5";
 
 export default function Header() {
   return (
-    <div className="flex justify-between items-center">
-      <div className="mt-16 ml-16">
+    <div className="w-full max-w-7xl flex justify-between items-center max-md:max-w-full max-md:flex-col max-md:justify-center">
+      <div className="mt-8 md:mt-16 md:ml-16">
         <div className="w-24 h-9 bg-[#D9D9D9] border border-solid border-[#000000] rounded-3xl flex items-center">
           <div className="w-7 h-7 bg-black rounded-full ml-1" />
         </div>
@@ -12,11 +12,11 @@ export default function Header() {
         </p>
       </div>
 
-      <div className="w-[800px] h-16 bg-[#D9D9D9] mt-16 mr-16 border border-solid border-[#000000] rounded-[40px] shadow-2xl flex items-center">
-        <IoSearch className="text-5xl text-gray-400 ml-6" />
+      <div className="w-[450px] bg-[#D9D9D9] mt-8 md:mt-16 md:mr-16 gap-4 pl-6 pr-9 py-2 border border-solid border-[#000000] rounded-[40px] shadow-2xl flex items-center">
+        <IoSearch className="text-5xl text-gray-400" />
         <input
           placeholder="Search for your preffered city..."
-          className="w-full h-full bg-transparent ml-3 outline-none text-[#292929] text-lg font-poppins placeholder-gray-500 focus:placeholder-gray-400 pr-8"
+          className="w-full h-full bg-transparent outline-none text-[#292929] text-lg font-poppins placeholder-gray-500 focus:placeholder-gray-400"
         />
       </div>
     </div>
